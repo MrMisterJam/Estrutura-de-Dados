418c7b711ec697687968f35a326a33562b2ea188
MrMisterJam <141333254+MrMisterJam@users.noreply.github.com>
2026-02-19 10:44:51 -0300

    Add files via upload

---
+++ 2026/main.c
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    /*
+    int a, b, temp;
+    printf("Digite o valor de A:\n");
+    scanf("%d", &a);
+    printf("Digite o valor de B:\n");
+    scanf("%d", &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("Novos valores: A = %d B = %d", a, b);
+    */
+    
+    /*
+    float nota, soma, media;
+    for(int i = 0; i < 4; i++) {
+        scanf("%f", &nota);
+        soma += nota;
+    }
+    media = soma / 4;
+    printf("%.2f", media);
+    */
+    /*
+    float real, cotacao, dolar;
+    
+    scanf("%f", &real);
+    scanf("%f", &cotacao);
+    dolar = real / cotacao;
+    printf("%.2f", dolar);
+    */
+    
+    
+    return 0;
+}
