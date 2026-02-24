c3bb5e8d9069e2342b5d7d95b924bfe317fb7a1a
MrMisterJam <141333254+MrMisterJam@users.noreply.github.com>
2026-02-24 08:42:40 -0300

    Add files via upload

--- Cassio/Lista_De_Exercicios_01/main.c
+++ Cassio/Lista_De_Exercicios_01/main.c
@@ -61,6 +61,7 @@ int main(void)
     printf("Mes selecionado: %s\n", meses[mes]);
     */
 
+    /*
     // 23)
     double num1, num2, result;
     char operador;
@@ -89,6 +90,69 @@ int main(void)
     }
 
     printf("%f", result);
+    */
+    
+    /*
+    // 24)
+    char idiom = pegar_char();
+    switch (idiom)
+    {
+    case 'E':
+        printf("Hello World!");
+        break;
+    case 'P':
+        printf("Ola Mundo!");
+        break;
+    case 'S':
+        printf("Hola Mundo!");
+        break;
+    default:
+        printf("Idioma invalido, encerrando o programa...");
+        exit(EXIT_FAILURE);
+        break;
+    }
+    */
+    
+    /*
+    // 25)
+    int pontuacao = 0;
+    char cor;
+    do {
+        printf("Pontuacao atual: %d\n", pontuacao);
+        cor = pegar_char();
+        switch(cor)
+        {
+        case 'V':
+            pontuacao += 10;
+            break;
+        case 'A':
+            pontuacao += 5;
+            break;
+        case 'Z':
+            pontuacao += 20;
+            break;
+        default:
+            printf("Cor invalida.\n");
+            break;
+        }
+        
+    } while (pontuacao < 100);
+    
+    printf("Voce ganhou o jogo!");
+    
+    */
+    
+    // 34)
+    
+    for (int i = 2; i <= 20; i += 2) {
+        printf("%d\n", i);
+    }
+    
+    for (int i = 2; i <= 20; i++) {
+        if (i % 2 == 0) {
+            printf("%d\n", i);
+        }
+    }
 
     return 0;
 }
